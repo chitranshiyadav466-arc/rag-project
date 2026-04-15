7b84587f5b47575b28018f22f6baaf2b2da58b57
Chitranshi Yadav <chitranshiyadav466@gmail.com>
2026-04-15 22:22:14 +0530

    Added API key authentication security

--- app.py
+++ app.py
@@ -4,22 +4,28 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+MY_SECRET_KEY = "rag12345"
+
+
 @app.route("/")
 def home():
-    return "API is running 🚀"
+    return "API is running "
 
-@app.route('/api/chat', methods=['POST'])
+
+@app.route("/api/chat", methods=["POST"])
 def chat():
     data = request.json
 
+    
+    api_key = request.headers.get("x-api-key")
+
+    if api_key != MY_SECRET_KEY:
+        return jsonify({"error": "Unauthorized"}), 401
+
+   
     if not data or "message" not in data:
         return jsonify({"error": "Message is required"}), 400
 
-    user_msg = data.get("message")
+    user_msg = data["message"]
 
-    return jsonify({
-        "reply": f"You said: {user_msg}"
-    })
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+    return jsonify({"reply": f"You said: {user_msg}"})
